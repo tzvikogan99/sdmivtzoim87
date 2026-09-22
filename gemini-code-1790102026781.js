693be407150b29cca9c440898f86aaacac275b8e
tzvikogan99 <tzvikogan99@gmail.com>
2026-09-22 14:34:10 -0400

    Add files via upload

---
+++ gemini-code-1790102026781.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "YOUR_APP_ID"
+};
